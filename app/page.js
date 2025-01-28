55d810110ba348d33665ea2fac6a44fa78b86f6d
antromo741 <anthonyroma741@gmail.com>
2025-01-28 13:29:58 -0500

    setup header & wallet connect

--- app/page.js
+++ app/page.js
@@ -5,10 +5,11 @@ import { getAssociatedTokenAddressSync } from "@solana/spl-token"
 import { AnchorProvider, Program } from "@coral-xyz/anchor"
 
 // Import config & IDL
-import config from "@/app/config.json"
-import Crowdsale from "@/app/idl/crowdsale.json"
+/* import config from "@/app/config.json"
+import Crowdsale from "@/app/idl/crowdsale.json" */
 
-
+// Import components
+import Header from "./components/Header"
 
 export default function Home() {
   const [provider, setProvider] = useState(null)
@@ -23,10 +24,9 @@ export default function Home() {
         setProvider(provider)
 
         provider.on("connect", async (publicKey) => {
-          // Setup connection to the cluster
+          // Setup connection to cluster
           const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
           const anchorProvider = new AnchorProvider(connection, publicKey)
-
 
           // Set the anchor connection & user
           setAnchorProvider(anchorProvider)
@@ -37,6 +37,7 @@ export default function Home() {
           // Fetch Crowdsale State
 
           // Fetch Balanace
+          await getUserBalance(anchorProvider)
         })
         provider.on("disconnect", () => {
           setUser(null)
@@ -48,9 +49,24 @@ export default function Home() {
     }
   }
 
+  const getUserBalance = async (anchorProvider) => {
+    // Setup public keys
+    const userPublicKey = new PublicKey(anchorProvider.wallet)
+
+    const userBalance = await anchorProvider.connection.getBalance(userPublicKey)
+    setUserBalance(userBalance)
+  }
+
+
+
+  useEffect(() => {
+    getProvider()
+  }, [])
+
 
   return (
     <div className="page">
+     <Header provider={provider} user={user} setUser={setUser} />
       <main className="main">
         <div className="hero">
           <h1>Introducing sDAPP</h1>

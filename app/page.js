79051123f7dd3c7c726787065b727e9ae7a24e88
antromo741 <anthonyroma741@gmail.com>
2025-01-28 19:31:11 -0500

    added addresses to config, add idl, pull in state

--- app/page.js
+++ app/page.js
@@ -5,16 +5,22 @@ import { getAssociatedTokenAddressSync } from "@solana/spl-token"
 import { AnchorProvider, Program } from "@coral-xyz/anchor"
 
 // Import config & IDL
-/* import config from "@/app/config.json"
-import Crowdsale from "@/app/idl/crowdsale.json" */
+import config from "@/app/config.json"
+import Crowdsale from "@/app/idl/crowdsale.json"
 
 // Import components
 import Header from "./components/Header"
 
 export default function Home() {
   const [provider, setProvider] = useState(null)
-  const [AnchorProvider, setAnchorProvider] = useState(null)
+  const [anchorProvider, setAnchorProvider] = useState(null)
   const [user, setUser] = useState(null)
+  const [userBalance, setUserBalance] = useState(0)
+  const [userTokenBalance, setUserTokenBalance] = useState(0)
+  const [crowdsaleProgram, setCrowdsaleProgram] = useState(null)
+  const [crowdsaleBalance, setCrowdsaleBalance] = useState(0)
+  const [crowdsaleTokenBalance, setCrowdsaleTokenBalance] = useState(0)
+  const [crowdsaleCost, setCrowdsaleCost] = useState(0)
 
   const getProvider = async () => {
     if ('phantom' in window) {
@@ -32,32 +38,63 @@ export default function Home() {
           setAnchorProvider(anchorProvider)
           setUser(publicKey)
 
-          // Get Crowdsale Program
+          // Initantiate the program
+          const crowdsaleProgram = new Program(
+            Crowdsale,
+            anchorProvider
+          )
+          setCrowdsaleProgram(crowdsaleProgram)
 
-          // Fetch Crowdsale State
+          // Fetch the current state of the crowdsale
+          const crowdsaleState = await crowdsaleProgram.account.crowdsale.fetch(config.CROWDSALE_PDA)
+          setCrowdsaleCost(crowdsaleState.cost)
 
-          // Fetch Balanace
+          // Fetch balances
           await getUserBalance(anchorProvider)
+          await getCrowdsaleBalance(anchorProvider)
         })
+
         provider.on("disconnect", () => {
           setUser(null)
         })
-
       }
-
-
     }
   }
 
   const getUserBalance = async (anchorProvider) => {
     // Setup public keys
     const userPublicKey = new PublicKey(anchorProvider.wallet)
+    const tokenPublicKey = new PublicKey(config.TOKEN_MINT_ACCOUNT)
 
+    // Get user's SOL balance
     const userBalance = await anchorProvider.connection.getBalance(userPublicKey)
     setUserBalance(userBalance)
+
+    // Get user's Token balance
+    // Since the user might have 0, we need to get their account info
+    const userTokenAccount = getAssociatedTokenAddressSync(tokenPublicKey, userPublicKey, true)
+    const userTokenAccountInfo = await anchorProvider.connection.getAccountInfo(userTokenAccount)
+
+    // If they have never had a balance, their account info will be null
+    if (userTokenAccountInfo) {
+      const userTokenBalance = await anchorProvider.connection.getTokenAccountBalance(userTokenAccount)
+      setUserTokenBalance(userTokenBalance.value.amount)
+    }
   }
 
+  const getCrowdsaleBalance = async (anchorProvider) => {
+    // Setup public keys
+    const crowdsalePDAKey = new PublicKey(config.CROWDSALE_PDA)
+    const crowdsalePDATokenKey = new PublicKey(config.CROWDSALE_PDA_TOKEN_ACCOUNT)
 
+    // Get Crowdsale's SOL balance
+    const crowdsaleBalance = await anchorProvider.connection.getBalance(crowdsalePDAKey)
+    setCrowdsaleBalance(crowdsaleBalance)
+
+    // Get Crowdsale's Token balance
+    const crowdsaleTokenBalance = await anchorProvider.connection.getTokenAccountBalance(crowdsalePDATokenKey)
+    setCrowdsaleTokenBalance(crowdsaleTokenBalance.value.amount)
+  }
 
   useEffect(() => {
     getProvider()
@@ -66,7 +103,7 @@ export default function Home() {
 
   return (
     <div className="page">
-     <Header provider={provider} user={user} setUser={setUser} />
+      <Header provider={provider} user={user} setUser={setUser} />
       <main className="main">
         <div className="hero">
           <h1>Introducing sDAPP</h1>
